f4c322a2bf5f6094e24b61629e0bf1e87f956ea8
shpark <zpxsch9001@naver.com>
2025-07-23 15:18:47 +0900

    Add getLetterCount

--- Project24/SimilarityChecker.cpp
+++ Project24/SimilarityChecker.cpp
@@ -1,3 +1,8 @@
+#include <string>
+
 class SimilarityChecker {
 public:
+	int getLetterCount(std::string letter) {
+		return letter.length();
+	}
 };
--- Project24/SimilarityChecker_test.cpp
+++ Project24/SimilarityChecker_test.cpp
@@ -1,8 +1,17 @@
 #include "gmock/gmock.h"
 #include "SimilarityChecker.cpp"
 
+class SimilarityCheckerFixture : public testing::Test {
+public:
+	SimilarityChecker checker;
+};
+
+TEST_F(SimilarityCheckerFixture, getLetterCount) {
+	EXPECT_EQ(5, checker.getLetterCount("12345"));
+	EXPECT_EQ(3, checker.getLetterCount("abc"));
+}
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
 }
-
